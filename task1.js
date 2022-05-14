e0feaa59ef8115557f52f627caa88523e4b35e30
Donatas <stonkusdd@gmail.com>
2022-05-14 17:29:10 +0300

    first task

---
+++ task1.js
@@ -0,0 +1,16 @@
+// Write a function that takes the average of an array of number and returns the nearest whole number.
+//Examples:
+//number([1, 2])                 => 2
+//number([2, 4])                 => 3
+//number([23, 18, 1, 1, 1, 1])   => 8
+//number([1, 2, 3, 4, 5])        => 3
+
+const arrayAverage = (numbersArr) => {
+  let sum = 0;
+
+  numbersArr.map((n) => (sum += n));
+
+  return Math.round(sum / numbersArr.length);
+};
+
+console.log(arrayAverage([23, 18, 1, 1, 1, 1]));
