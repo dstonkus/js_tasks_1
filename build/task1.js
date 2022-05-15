03985bab0f2bdde9e1221757304d734d17e3ec81
Donatas <stonkusdd@gmail.com>
2022-05-15 23:49:17 +0300

    less space

--- build/task1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.arrayAverage = void 0;
-var arrayAverage = function (numbersArr) {
-    return Math.round(numbersArr.reduce(function (prevValue, currValue) { return prevValue + currValue; }) /
-        numbersArr.length);
-};
-exports.arrayAverage = arrayAverage;
